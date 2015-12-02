05bcaa67237212cf7a33c470980b0d4fda7f8f6f
rhandyrhoads <gcosteas@gmail.com>
2015-12-01 22:58:08 -0500

    Fixed codeGeneration and started implementation of guessing.
    Started working on input.

--- Crack/src/crack/Crack.java
+++ Crack/src/crack/Crack.java
@@ -11,16 +11,35 @@ public class Crack{
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		String code = generateCode();
-		System.out.println(code);
 		
-//		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-//	    executorService.scheduleAtFixedRate(Crack::clue, 0, 1, TimeUnit.SECONDS);
-		
+		System.out.println("Let the game begin! I'm thinking of a 6 numbers code.");
+		System.out.println("Numbers in the code are in the range of 0 to 6. Clues will be given."
+				+ "\nGame will end after 25 clues. Good luck!!");
+		int i = 0;
+		boolean gameWon = false;
+		String guess = scan.next();
+		while (i < 25 && !gameWon){
+			clue();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (guess == code){
+				gameWon = true;
+			}
+			i++;
+		}
+		if (gameWon){
+			System.out.println("Congratulations! You guessed correctly.");
+		}
+		scan.close();
 	}
 	private static void clue(){
 		System.out.println("Clue: the sky is cool: ");
 	}
-	private String generateCode(){
+	private static String generateCode(){
 		Random rand = new Random();
 		String code = "";
 		for (int i = 0; i < 6; i++){
@@ -28,4 +47,5 @@ public class Crack{
 		}
 		return code;
 	}
+	
 }
