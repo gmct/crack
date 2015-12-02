f66efe4b85d17b7fc6bc10a0c8ef338299a7f7b8
rhandyrhoads <gcosteas@gmail.com>
2015-12-01 23:39:42 -0500

    Implemented initial concurrency work
    Desired function involves clues continuing while allowing any amount of
    inputs.  Concurrency will allow this.  Outstanding problem being that
    the main thread won't quit.  Actively looking into solution.

--- Crack/src/crack/Crack.java
+++ Crack/src/crack/Crack.java
@@ -7,29 +7,30 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 
-public class Crack{
+public class Crack implements Runnable{
+	static Random rand = new Random();
+	static boolean gameOver = false;
+	
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		String code = generateCode();
 		
+		System.out.println(code);
+		
 		System.out.println("Let the game begin! I'm thinking of a 6 numbers code.");
 		System.out.println("Numbers in the code are in the range of 0 to 6. Clues will be given."
 				+ "\nGame will end after 25 clues. Good luck!!");
-		int i = 0;
+		
+		String guess;
 		boolean gameWon = false;
-		String guess = scan.next();
-		while (i < 25 && !gameWon){
-			clue();
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+	    boolean gameOver = false;
+		(new Thread(new Crack())).start();
+		while (!gameOver){
+			guess = scan.next();
 			if (guess == code){
 				gameWon = true;
+				gameOver = true;
 			}
-			i++;
 		}
 		if (gameWon){
 			System.out.println("Congratulations! You guessed correctly.");
@@ -38,14 +39,28 @@ public class Crack{
 	}
 	private static void clue(){
 		System.out.println("Clue: the sky is cool: ");
+		
 	}
 	private static String generateCode(){
-		Random rand = new Random();
 		String code = "";
 		for (int i = 0; i < 6; i++){
 			code += rand.nextInt(7);
 		}
 		return code;
+	}
+	@Override
+	public void run() {
+		int i = 0;
+		while (i < 25 && !gameOver){
+			clue();
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			i++;
+		}
+		gameOver = true;
 	}
 	
 }
